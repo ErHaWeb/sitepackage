7aa6078ec7eb9e7b1c66e8366cef8fea8894e5c0
Eric Bode <eric.bode@gmx.de>
2022-09-22 16:10:16 +0200

    Small fixes

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -16,7 +16,7 @@
 declare(strict_types=1);
 
 /**
- * Register custom Icons in the IconRegistry provided by EXT:sitepackage
+ * Registers custom Icons in the IconRegistry provided by EXT:sitepackage
  *
  * https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ExtensionArchitecture/FileStructure/Configuration/Icons.html
  */
