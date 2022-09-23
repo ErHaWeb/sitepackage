232addeb6a895055b9f1a7c8098764940f48f59c
Eric Bode <eric.bode@gmx.de>
2022-09-23 18:32:11 +0200

    Code refactoring using TYPO3 Rector

--- Configuration/ExpressionLanguage.php
+++ Configuration/ExpressionLanguage.php
@@ -21,8 +21,10 @@ declare(strict_types=1);
  * https://docs.typo3.org/m/typo3/reference-coreapi/11.5/en-us/ApiOverview/SymfonyExpressionLanguage/Index.html#registering-new-provider-within-an-extension
  */
 
+use VendorName\Sitepackage\ExpressionLanguage\CustomTypoScriptConditionProvider;
+
 return [
     'typoscript' => [
-        \VendorName\Sitepackage\ExpressionLanguage\CustomTypoScriptConditionProvider::class,
+        CustomTypoScriptConditionProvider::class,
     ]
 ];
