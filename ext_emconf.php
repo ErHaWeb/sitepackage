b335dab611a1f17d5068d131cf02366e13c7b102
Eric Bode <eric.bode@gmx.de>
2022-10-05 12:12:00 +0200

    Update version constraints for TYPO3 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,10 +28,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Author Company',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '12.0.0-12.5.99',
         ],
         'conflicts' => [
         ],
