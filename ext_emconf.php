c688a545d1894b045e46e2a55324383cbbb11b80
Eric Harrer <info@eric-harrer.de>
2024-10-18 09:42:19 +0200

    [TASK] Update version requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.5.99',
+            'typo3' => '13.4.0-13.4.99',
         ],
         'conflicts' => [
         ],
