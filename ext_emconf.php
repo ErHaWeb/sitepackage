b28a34b0c23ad92fd6f90b01fe60263f80413e20
Eric Harrer <info@eric-harrer.de>
2026-01-19 13:03:09 +0100

    [TASK] Improve version range in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.0.0-14.99.99',
+            'typo3' => '14.0.0-14.3.99',
         ],
         'conflicts' => [
         ],
